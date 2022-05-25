2038c87265145ce30f0eb1b1d33f93d9e7b52f78
Pablo Macuada <pablo.macuada@usach.cl>
2022-05-25 00:59:20 -0400

    [Pablo Macuada] - Se crean los modelos de las tablas que no tienen llaves foráneas

---
+++ Lab1_DBD/database/migrations/2022_05_25_044708_create_card__types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('card__types', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_tipo',30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('card__types');
+    }
+};
