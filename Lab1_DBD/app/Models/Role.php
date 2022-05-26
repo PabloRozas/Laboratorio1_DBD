174f78ab4329179ae407ebdbca96fdef1b551ebc
Hans <hans.villarroel@usach.cl>
2022-05-26 00:56:40 -0400

    [Hans Villarroel] - Se hace relacion M:M func-rol

--- Lab1_DBD/app/Models/Role.php
+++ Lab1_DBD/app/Models/Role.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+    public function funciones()
+    {
+        return $this->belongsToMany(Functionality::class);
+    }
 }
