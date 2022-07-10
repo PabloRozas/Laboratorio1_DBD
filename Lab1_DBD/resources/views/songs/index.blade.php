4d31ed1175a3948117815617abc210518e6b251d
Hans <hans.villarroel@usach.cl>
2022-07-10 00:40:27 -0400

    [Hans Villarroel] UI editar user/ mi-perfil en Index

--- Lab1_DBD/resources/views/songs/index.blade.php
+++ Lab1_DBD/resources/views/songs/index.blade.php
@@ -1,91 +1,94 @@
-@if(Session::has('mensaje'))
-{{ Session::get('mensaje') }}
+@if (Session::has('mensaje'))
+    {{ Session::get('mensaje') }}
 @endif
 @extends('layouts.main')
 @section('content')
 
-<body>
+    <body>
 
-    {{-- Grid art --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class=" ms-2 navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        {{-- Grid art --}}
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class=" ms-2 navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                @if (Auth::check())
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{ Auth::user()->name }}
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
-                    </div>
-                </li>
-                @if (@Auth::user()->hasRole(['admin', 'artist']))
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('songs/create') }}"> Registrar nueva canción </a>
-                </li>
-                @endif
-                @endif
-            </ul>
-            <div class="input-group w-25 ms-auto me-5 justify-content-end ">
-                <input type="text" class="form-control" aria-label="Text input with dropdown button">
-                <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                    aria-expanded="false">Dropdown</button>
-                <ul class="dropdown-menu dropdown-menu-end">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                     </li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    @if (Auth::check())
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}">Logout</a>
+                            </div>
+                        </li>
+                        @if (@Auth::user()->hasRole(['admin', 'artist']))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('songs/create') }}"> Registrar nueva canción </a>
+                            </li>
+                        @endif
+                    @endif
                 </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="container mt-3">
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            @foreach ($songs as $song)
-            <div class="col">
+                <div class="input-group w-25 ms-auto me-5 justify-content-end ">
+                    <form action="{{ route('songs.filter') }}" method="GET">
+                        <input type="text" name="genre" class="form-control" aria-label="Text input with dropdown button" value="{{$genre ?? ''}}">
 
-                <div class="card h-50">
-                    <img src="{{ asset($song->foto) }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $song->titulo }}</h5>
-                        <p class="card-text">{{ $song->duracion }}</p>
-
-                    </div>
-                    <div class="d-grid gap-2 col-6 mx-auto mb-3">
-                        <audio controls id = "music">
-                            <source src="{{ asset($song->url_cancion) }}" type= "audio/mpeg">
-                        </audio>
-                        <button id="eventoPlay" class="btn btn-lg btn-primary" type="button"> ▶️ </button>
-                        <a href=" {{ url('/songs/'.$song->id.'/edit') }}">Editar</a>
-                        <form action="{{url('/songs/'.$song->id)}}" method="post">
-                            @csrf
-                            {{ method_field('DELETE') }}
-                            <input type="submit" onclick="return confirm('¿Seguro que deseas borrar?')" value="Borrar">
-                        </form>
-                    </div>
+                        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">Dropdown</button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                        </ul>
+                    </form>
                 </div>
-
             </div>
-            @endforeach
+        </nav>
+        <div class="container mt-3">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                @foreach ($songs as $song)
+                    <div class="col">
+
+                        <div class="card h-50">
+                            <img src="{{ asset($song->foto) }}" class="card-img-top h-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $song->titulo }}</h5>
+                                <p class="card-text">{{ $song->duracion }}</p>
+
+                            </div>
+                            <div class="d-grid gap-2 col-6 mx-auto mb-3">
+                                <audio controls id="music">
+                                    <source src="{{ asset($song->url_cancion) }}" type="audio/mpeg">
+                                </audio>
+                                <a href=" {{ url('/songs/' . $song->id . '/edit') }}">Editar</a>
+                                <form action="{{ url('/songs/' . $song->id) }}" method="post">
+                                    @csrf
+                                    {{ method_field('DELETE') }}
+                                    <input type="submit" onclick="return confirm('¿Seguro que deseas borrar?')"
+                                        value="Borrar">
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                @endforeach
+            </div>
         </div>
+        </div>
+    </body>
     </div>
-    </div>
-</body>
-</div>
 @endsection
--- Lab1_DBD/resources/views/users/usuario.blade.php
+++ Lab1_DBD/resources/views/users/usuario.blade.php
@@ -1,32 +1,65 @@
-
 @extends('layouts.app')
 @section('content')
-<div class="container">
+    <div class="container">
 
-    <div>
-        <ol>
-        <h2>MI PERFIL</h2>
-            <li>{{Auth::user()->name}}</li>
-            <li>{{Auth::user()->username}}</li>
-            <li>{{Auth::user()->email}}</li>
-            <li>{{Auth::user()->getAge()}} </li>
-            <li>{{Auth::user()->fecha_nacimiento}}</li>
-            <li>{{Auth::user()->created_at}}</li>
-            @if (Auth::user()->num_tarjeta != null)
-            <li>{{Auth::user()->num_tarjeta}}</li>
-            @endif
+        <div>
+            <ol>
+                <h2>Mi Perfil</h2>
+                <ul class="list-group list-group-horizontal ">
+                    <li class="list-group-item flex-fill "> Mi nombre: </li>
+                    <li class="list-group-item flex-fill ">{{ Auth::user()->name }}</li>
+                </ul>
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item flex-fill "> Nombre de Usuario: </li>
+                    <li class="list-group-item flex-fill ">{{ Auth::user()->username }}</li>
+                </ul>
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item flex-fill "> Correo Electronico: </li>
+                    <li class="list-group-item flex-fill ">{{ Auth::user()->email }}</li>
+                </ul>
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item flex-fill "> Mi edad: </li>
+                    <li class="list-group-item flex-fill ">{{ Auth::user()->getAge() }} </li>
+                </ul>
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item flex-fill "> Fecha de nacimiento: </li>
+                    <li class="list-group-item flex-fill ">{{ Auth::user()->fecha_nacimiento }}</li>
+                </ul>
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item flex-fill "> Cuenta creada en : </li>
+                    <li class="list-group-item flex-fill ">{{ Auth::user()->created_at }}</li>
+                </ul>
+                @if (Auth::user()->num_tarjeta != null)
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item flex-fill "> Tarjeta Asociada : </li>
+                        <li class="list-group-item flex-fill ">{{ Auth::user()->num_tarjeta }}</li>
+                    </ul>
+                @endif
 
-            <li>{{Auth::user()->roles->pluck('name')->first()}}</li>
-            <li>@if (Auth::user()->suscripcion == 1)
-                    'Subscrito'
-                    @else
-                    'No subscrito'
-                    @endif
-            </li>
-            <a class="btn btn-primary" aria-current="page" href="{{ url('/users/'.Auth::user()->id.'/edit') }}">Editar Perfil</a>
-        </ol>
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item flex-fill "> Mi rol: </li>
+                    <li class="list-group-item flex-fill ">{{ Auth::user()->roles->pluck('name')->first() }}</li>
+                </ul>
+
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item flex-fill "> Estado Subscripcion: </li>
+                    <li class="list-group-item flex-fill">
+                        @if (Auth::user()->suscripcion == 1)
+                            Subscrito
+                        @else
+                            No subscrito
+                        @endif
+                    </li>
+                </ul>
+                <div class= mt-5>
+                     <a class="btn btn-primary" aria-current="page"
+                        href="{{ url('/users/' . Auth::user()->id . '/edit') }}">Editar
+                        Perfil</a>
+                </div>
+
+            </ol>
 
 
+        </div>
     </div>
-</div>
 @endsection
